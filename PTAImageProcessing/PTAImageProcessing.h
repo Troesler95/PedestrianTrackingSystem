02d99cdd06429a4d541966062822576207995c21
Tyler Roesler <troesler95@gmail.com>
2017-12-14 11:18:19 -0500

    Integrate Useful Code from ImageProcessing Branch (#5)
    * Created a project to build and test the IP system to be later converted to a library
    
    * Improved performance/logic of background subtractor
    
    * Added bounding box funtionality and separated the code into functions for cleaner-looking code and more reuse. See slack for additional comments.
    
    * Added YAML parsing function to store the truth data in a map structure. Added the truth bounding box to the image in red.
    
    * Pedestrian tracking using OpenCV's diamler pedestrian tracker with multi-scale detection
    
    * moved code to static library
    
    * Increased performance of detector by converting to graycale
    
    * removed test app from commit tree. convert image to graycale before prcoessing to increase performance
    
    * convert image to graycale before prcoessing to increase performance
    
    * removed test application from IP branch

--- PTAImageProcessing/PTAImageProcessing.h
+++ PTAImageProcessing/PTAImageProcessing.h
@@ -1,1 +1,31 @@
 ﻿#pragma once
+#include <vector>
+#include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
+
+namespace PTA_ImageProcessing
+{
+	typedef enum {
+		DEFAULT,
+		DIALMER
+	} PTADetectType;
+
+	/** PedestrianDetectOpenCV
+	 * @description - 
+	 */
+	class PedestrianDetectOpenCV
+	{
+	private:
+		cv::HOGDescriptor _hog;
+		bool _daimlerFlag = false;
+	public:
+		PedestrianDetectOpenCV();
+		// @param - type of detector to use
+		// @param - whether or not to use background detector
+		PedestrianDetectOpenCV(PTADetectType);
+		// @param - existing hog descriptor
+		PedestrianDetectOpenCV(cv::HOGDescriptor);
+		// @param - inImage
+		std::vector<cv::Rect> Detect(cv::Mat);
+	};
+}
